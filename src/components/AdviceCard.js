8560ad2abb680299f957866fc076d7f82008e857
mylliejohnson <mylliejohnson@gmail.com>
2022-04-09 10:56:36 -0400

    deployed

---
+++ src/components/AdviceCard.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function AdviceCard(props) {
+
+    // https://api.adviceslip.com/advice
+
+    const [advice, setAdvice] = useState([])
+
+    useEffect(() => {
+        axios.get("https://api.adviceslip.com/advice").then((res) => {
+            setAdvice(res.data.slip)
+            console.log(res.data.slip)
+        })
+    }, [])
+
+    return (
+        <div className='advice-card'>
+            <div>
+                <p id="advice-id">Advice #{advice.id}</p>
+                <h3 id="advice">"{advice.advice}"</h3>
+                <img src="/images/pattern-divider-mobile.svg" />
+                <button onClick={() => window.location.reload()}>
+                    <img id="btn-dice" src="/images/icon-dice.svg" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default AdviceCard;
